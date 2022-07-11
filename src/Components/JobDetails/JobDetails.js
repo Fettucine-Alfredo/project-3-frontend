2f7debb4dedf0380e1ebf7210ed256a6e9e9f503
StarlightStingray <somajstingray@gmail.com>
2022-07-11 16:46:00 -0700

    Rebasing

--- src/Components/JobDetails/JobDetails.js
+++ src/Components/JobDetails/JobDetails.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 function JobDetails(props) {
+	const [formState, setFormState] = useState('');
+	let [error, setError] = useState(null);
+
+	useEffect(() => {
+		fetch(
+			`mongodb+srv://dbuser:ZLBDBYXtJNAVlSbr@sei-59.liigl.mongodb.net/JobsList?retryWrites=true&w=majority`
+		)
+			.then((res) => res.json())
+			.then((jobDeets) => setFormState(jobDeets))
+			.catch(setError);
+	}, []);
+
+	console.log(formState);
+
 	return <div></div>;
 }
 
