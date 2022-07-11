f4473ce2ebf0972ba698ca058ae73627611c5ddc
alfredo-saenz <alfredosaenz104@gmail.com>
2022-07-11 16:35:07 -0500

    Made a form for user to put in username

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Home(props) {
-	return <div></div>;
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+
+	function handleUserChange(event) {
+		event.preventDefault();
+		setName(event.target.value);
+	}
+	function handleEmailChange(event) {
+		event.preventDefault();
+		setEmail(event.target.value);
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+	}
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<h3>Username:</h3>
+				<input
+					id='username'
+					placeholder='Username'
+					type='text'
+					onChange={handleUserChange}
+					required
+				/>
+				<button type='submit'>login</button>
+			</form>
+		</div>
+	);
 }
 
 export default Home;
