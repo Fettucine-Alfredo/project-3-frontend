afa7ac562581afc09e490f54b0d5f4cc73890928
ShivalikSharma98 <Shivaliksharma98@gmail.com>
2022-07-11 17:53:39 -0400

    Add Axios

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import './Header.css';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
 
-function Header(props) {
-	return <div></div>;
+function Header() {
+	return (
+		<Navbar className='color-nav' variant='light' expand='lg'>
+			<Container>
+				<LinkContainer to='/'>
+					<Navbar.Brand className='trakr'>Trakr.</Navbar.Brand>
+				</LinkContainer>
+				<Navbar.Toggle aria-controls='basic-navbar-nav' />
+				<Navbar.Collapse id='basic-navbar-nav'>
+					<Nav className='me-auto'>
+						<LinkContainer to='/'>
+							<Nav.Link>Home</Nav.Link>
+						</LinkContainer>
+						<LinkContainer to='/about'>
+							<Nav.Link>About</Nav.Link>
+						</LinkContainer>
+						<NavDropdown title='Account' id='basic-nav-dropdown'>
+							<NavDropdown.Item href='#action/3.1'>
+								Create Account
+							</NavDropdown.Item>
+							<NavDropdown.Item href='#action/3.2'>Login</NavDropdown.Item>
+							<NavDropdown.Item href='#action/3.3'>Logout</NavDropdown.Item>
+						</NavDropdown>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 }
 
 export default Header;
