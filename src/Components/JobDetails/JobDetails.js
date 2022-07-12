6170d56b0a94362b1238ef2219ca0a10acf4dc4a
StarlightStingray <somajstingray@gmail.com>
2022-07-12 14:55:24 -0700

    Make axios call for database

--- src/Components/JobDetails/JobDetails.js
+++ src/Components/JobDetails/JobDetails.js
@@ -1,69 +1,56 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+// Import the config object from Constants
 import { config } from '../../Constants';
+import axios from 'axios';
 import './jobDetails.css';
 
 function JobDetails(props) {
-	const [formState, setFormState] = useState('');
+	const [job, setJob] = useState('');
 	let [error, setError] = useState(null);
-	const { id } = useParams();
+	const { username, id } = useParams();
+	console.log(username, id);
+	// use config.API_URL to get the base URL and then add whatever endpoint to the base URL.
+	const url = `${config.API_URL}/user/${username}/jobs/${id}`;
 
-	const job = {
-		company: {
-			name: 'Facebook',
-		},
-		title: 'Junior FrontEnd Developer',
-		url: 'https://facebook.com',
-		description:
-			"We're looking for a junior developer that has 500 years of experience in JavaScript, React, React Redux, Gatsby, Express, MongoDB, Mongoose, Python, C++, C#, HTML, CSS, and every other framework/library ever written",
-		skills: ['Node', 'React', 'Express', 'MondoDB'],
-		contacts: [
-			{
-				name: 'Mark Zuckerburger',
-				email: 'mark@facebook.com',
-				_id: '62cca2933388e8e80e262df5',
-			},
-			{
-				name: 'Steve',
-				email: 'steve@facebook.com',
-				_id: '62cca2933388e8e80e262df6',
-			},
-		],
-		_id: '62cca2933388e8e80e262df4',
-		createdAt: '2022-07-11T22:22:11.445Z',
-		updatedAt: '2022-07-11T22:22:11.445Z',
-	};
-
+	// Make a request for a user with a given ID
 	useEffect(() => {
-		fetch(`http://localhost:8000/api/user/jobs/${id}`)
-			.then((res) => res.json())
-			.then((jobDeets) => setFormState(jobDeets))
-			.catch(setError);
+		axios
+			.get(url)
+			.then(function (response) {
+				// handle success
+				console.log(response);
+				setJob(response.data);
+			})
+			.catch(function (error) {
+				// handle error
+				console.log(error);
+			});
 	}, []);
 
-	const skillsList = job.skills.map((skill) => {
-		return <li>{skill}</li>;
-	});
-	const contactsList = job.contacts.map((contact) => {
+	if (job) {
+		const skillsList = job.skills.map((skill, index) => {
+			return <li key={`skill-${index}`}>{skill}</li>;
+		});
+		const contactsList = job.contacts.map((contact, index) => {
+			return (
+				<li key={`contact-${index}`}>
+					{contact.name}, {contact.email}
+				</li>
+			);
+		});
+
 		return (
-			<li>
-				{contact.name}, {contact.email}
-			</li>
+			<div className='jobDetails'>
+				<h1>{job.company.name}</h1>
+				<h3>'{job.title}'</h3>
+				<h3>{job.url}</h3>
+				<p>{job.description}</p>
+				<ul>Skills:{skillsList}</ul>
+				<ul>Job Contacts: {contactsList}</ul>
+			</div>
 		);
-	});
-
-	console.log(formState);
-
-	return (
-		<div className='jobDetails'>
-			<h1>{job.company.name}</h1>
-			<h3>{job.title}</h3>
-			<h3>{job.url}</h3>
-			<p>{job.description}</p>
-			<ul>Skills:{skillsList}</ul>
-			<ul>Job Contacts: {contactsList}</ul>
-		</div>
-	);
+	}
 }
 
 export default JobDetails;
