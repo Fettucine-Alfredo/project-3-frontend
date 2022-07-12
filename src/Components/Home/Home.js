ab817359a7a8cbffa36afec133ad6d5660a6048f
alfredo-saenz <alfredosaenz104@gmail.com>
2022-07-12 12:23:44 -0500

    Added two components, one is a sign up form and the other is a login form for the homepage.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,34 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
+import LoginForm from './LoginForm';
 
 function Home(props) {
-	const [name, setName] = useState('');
-	const [email, setEmail] = useState('');
-
-	function handleUserChange(event) {
-		event.preventDefault();
-		setName(event.target.value);
-	}
-	function handleEmailChange(event) {
-		event.preventDefault();
-		setEmail(event.target.value);
-	}
-
-	function handleSubmit(event) {
-		event.preventDefault();
-	}
 	return (
 		<div>
-			<form onSubmit={handleSubmit}>
-				<h3>Username:</h3>
-				<input
-					id='username'
-					placeholder='Username'
-					type='text'
-					onChange={handleUserChange}
-					required
-				/>
-				<button type='submit'>login</button>
-			</form>
+			<LoginForm />
 		</div>
 	);
 }
